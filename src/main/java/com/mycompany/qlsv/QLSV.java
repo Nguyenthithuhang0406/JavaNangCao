a31c0d1d214aad4c20ba052f3f0819b6a2d3cdca
NguyenThiThuHang0406 <hangthongminh04062004@gmail.com>
2025-03-17 14:56:34 +0700

    TH1-GUI

---
+++ src/main/java/com/mycompany/qlsv/QLSV.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.qlsv;
+
+/**
+ *
+ * @author Admin
+ */
+public class QLSV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
